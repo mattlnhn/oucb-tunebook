a1396510d1c65a0b00b0906ee81b5f2925a08f38
Matt Lenahan <matt.lenahan@eng.ox.ac.uk>
2025-08-30 18:26:05 +0100

    initial commit

---
+++ out-abc.sh
@@ -0,0 +1,87 @@
+abcm2ps -O = -s 0.7   out-on-the-ocean.abc 
+ps2eps -f out-on-the-ocean.ps
+epstopdf out-on-the-ocean.eps
+abcm2ps -O = -s 0.7   tripping-up-the-stairs.abc 
+ps2eps -f tripping-up-the-stairs.ps
+epstopdf tripping-up-the-stairs.eps
+abcm2ps -O = -s 0.7   the-rose-in-the-heather.abc 
+ps2eps -f the-rose-in-the-heather.ps
+epstopdf the-rose-in-the-heather.eps
+abcm2ps -O = -s 0.7   the-spotted-dog.abc 
+ps2eps -f the-spotted-dog.ps
+epstopdf the-spotted-dog.eps
+abcm2ps -O = -s 0.7   the-first-pint.abc 
+ps2eps -f the-first-pint.ps
+epstopdf the-first-pint.eps
+abcm2ps -O = -s 0.7   joe-derranes.abc 
+ps2eps -f joe-derranes.ps
+epstopdf joe-derranes.eps
+abcm2ps -O = -s 0.7   ninas.abc 
+ps2eps -f ninas.ps
+epstopdf ninas.eps
+abcm2ps -O = -s 0.7   the-picc-line.abc 
+ps2eps -f the-picc-line.ps
+epstopdf the-picc-line.eps
+abcm2ps -O = -s 0.7   the-soup-dragon.abc 
+ps2eps -f the-soup-dragon.ps
+epstopdf the-soup-dragon.eps
+abcm2ps -O = -s 0.7   the-lisnagun.abc 
+ps2eps -f the-lisnagun.ps
+epstopdf the-lisnagun.eps
+abcm2ps -O = -s 0.7   the-gathering.abc 
+ps2eps -f the-gathering.ps
+epstopdf the-gathering.eps
+abcm2ps -O = -s 0.7   the-handsome-young-maidens.abc 
+ps2eps -f the-handsome-young-maidens.ps
+epstopdf the-handsome-young-maidens.eps
+abcm2ps -O = -s 0.7   the-rambler.abc 
+ps2eps -f the-rambler.ps
+epstopdf the-rambler.eps
+abcm2ps -O = -s 0.7   health-to-the-ladies.abc 
+ps2eps -f health-to-the-ladies.ps
+epstopdf health-to-the-ladies.eps
+abcm2ps -O = -s 0.7   rosewood.abc 
+ps2eps -f rosewood.ps
+epstopdf rosewood.eps
+abcm2ps -O = -s 0.7   myras.abc 
+ps2eps -f myras.ps
+epstopdf myras.eps
+abcm2ps -O = -s 0.7   willie-colemans.abc 
+ps2eps -f willie-colemans.ps
+epstopdf willie-colemans.eps
+abcm2ps -O = -s 0.7   mcgoldricks.abc 
+ps2eps -f mcgoldricks.ps
+epstopdf mcgoldricks.eps
+abcm2ps -O = -s 0.7 -d 40pt   the-pipe-on-the-hob.abc 
+ps2eps -f the-pipe-on-the-hob.ps
+epstopdf the-pipe-on-the-hob.eps
+abcm2ps -O = -s 0.7 -d 40pt   the-cook-in-the-kitchen.abc 
+ps2eps -f the-cook-in-the-kitchen.ps
+epstopdf the-cook-in-the-kitchen.eps
+abcm2ps -O = -s 0.7 -d 40pt   the-random.abc 
+ps2eps -f the-random.ps
+epstopdf the-random.eps
+abcm2ps -O = -s 0.7 -d 40pt   merrily-kiss-the-quaker.abc 
+ps2eps -f merrily-kiss-the-quaker.ps
+epstopdf merrily-kiss-the-quaker.eps
+abcm2ps -O = -s 0.7 -d 40pt   grainnes.abc 
+ps2eps -f grainnes.ps
+epstopdf grainnes.eps
+abcm2ps -O = -s 0.7 -d 40pt   banish-misfortune.abc 
+ps2eps -f banish-misfortune.ps
+epstopdf banish-misfortune.eps
+abcm2ps -O = -s 0.7 -d 40pt   dusty-windowsills.abc 
+ps2eps -f dusty-windowsills.ps
+epstopdf dusty-windowsills.eps
+abcm2ps -O = -s 0.7 -d 40pt   the-cats-meow.abc 
+ps2eps -f the-cats-meow.ps
+epstopdf the-cats-meow.eps
+abcm2ps -O = -s 0.7 -d 40pt   den-toppede-hone-fra-vensyssel.abc 
+ps2eps -f den-toppede-hone-fra-vensyssel.ps
+epstopdf den-toppede-hone-fra-vensyssel.eps
+abcm2ps -O = -s 0.7 -d 40pt   kadril-van-mechelen.abc 
+ps2eps -f kadril-van-mechelen.ps
+epstopdf kadril-van-mechelen.eps
+abcm2ps -O = -s 0.7   james-browns.abc 
+ps2eps -f james-browns.ps
+epstopdf james-browns.eps
